cdae2d17a7fa5f40b80acd2ae44168cfbcd97edf
david.salle <david.salle@ensemblescolaire-niort.com>
2024-10-19 17:34:23 +0200

    Ajout annotations type et modifications routes

--- main.py
+++ main.py
@@ -2,16 +2,20 @@
 Simulateur de journal lumineux ou écran d'accueil
 Application web
 18/10/2024
+
+curl -X POST https://reqbin.com/echo/post/json
+  -H 'Content-Type: application/json'
+  -d '{"login":"my_login","password":"my_password"}'
 """
 
 # Librairie(s) utilisée(s)
 from flask import Flask, request, render_template, jsonify
-from screen import Screen
+from screen import Screen, ScreenError
    		
 
 # Création des objets de l'application
 app = Flask(__name__)
-screen = Screen("Here your scrolling message...", "#000000", 10, 1)
+screen = Screen("Here is your scrolling message...", "#000000", 10, 1)
 
 
 # Définitions des routes
@@ -23,18 +27,58 @@ def index():
         base_url=request.base_url
     )
 
-@app.route("/api/get-state")
+@app.route("/api/json/get-state")
 def get_state():
     reponse = {
+        "status": "OK",
         "message": screen.get_message(),
         "color": screen.get_color(),
         "speed": screen.get_speed()
     }
     return jsonify(reponse)
 
-@app.route("/api/set-message", methods=["POST"])
+@app.route("/api/json/set-message", methods=["POST", "GET"])
 def set_message_from_api():
-    pass
+    """Gère le changement de message
+    
+    curl -X POST http://127.0.0.1:5000/api/set-message \
+    -H 'Content-Type: application/json' \
+    -d '{"message":"Hello world les amis"}'
+    """
+    if request.method == "POST":
+        # Récupération des données JSON
+        donnees = request.json
+        nouveau_message = donnees.get("message")
+
+        if nouveau_message is not None:
+            try:
+                screen.set_message(nouveau_message)
+                reponse = {
+                    "status": "OK",
+                    "error": "...",
+                }
+                return jsonify(reponse)
+            except ScreenError as erreur:
+                reponse = {
+                    "status": "NOK",
+                    "error": erreur.message,
+                }
+                return jsonify(reponse)
+        else:
+            reponse = {
+                "status": "NOK",
+                "error": "Cannot find message in JSON",
+            }
+            return jsonify(reponse)
+    else:
+        reponse = {
+            "status": "NOK",
+            "error": "Only POST method is allowed",
+        }
+        return jsonify(reponse)
+    
+
+        
 
 
 # Lancement
--- screen.py
+++ screen.py
@@ -4,6 +4,19 @@ Gestion des paramètres de l'écran
 18/10/2024
 """
 
+# Librarie(s) utilisée(s)
+import re
+
+
+class ScreenError(Exception):
+    """Matérialise une erreur d'utilisation avec l'écran"""
+    
+    def __init__(self, message):
+        """Initialise l'erreur"""
+        self.message = message
+        super().__init__(self.message)
+
+
 class Screen:
     """Représente l'écran et ses paramètres"""
 
@@ -14,20 +27,33 @@ class Screen:
         self.speed = speed
         self.size = size
 
-    def get_message(self):
+    def get_message(self) -> str:
         return self.message
     
-    def get_color(self):
+    def get_color(self) -> tuple:
         return self.color
+
+    def get_speed(self) -> int:
+        return self.speed    
     
-    def get_speed(self):
-        return self.speed
-    
-    def get_size(self):
+    def get_size(self) -> int:        
         return self.size
+
          
-    def set_message(self, new_message):
-        self.message = new_message    
+    def set_message(self, new_message: str) -> None:
+        """Modifie le message affiché par l'écran
+         
+        Liste des contraintes générant une erreur
+        - longueur maximum = 50 caractères
+        - uniquement des chiffres et des lettres (minuscules ou majuscules)
+        """
+        #print( re.search(r"[^ \w+]", new_message) )
+        if len(new_message) > 50:
+            raise ScreenError("Message is too long")
+        elif re.search(r"[^ \w+]", new_message) is not None:
+            raise ScreenError("Message must contains only numbers and letters")
+        else:
+            self.message = new_message
     
     def set_color(self, new_color):
         self.color = new_color
@@ -37,3 +63,12 @@ class Screen:
 
     def set_size(self, new_size):
         self.size = new_size
+
+
+if __name__ == "__main__":
+    # Tests unitaires
+    s1 = Screen("...", (255, 0, 0), 10, 1)
+
+    s1.set_message("Hello world")
+    #s1.set_message("sdddddddddddddddddddddddddddddddddddddddtggggggggggggggggggggggggggggst")
+    #s1.set_message("<script></script>")
