573665de57e5e1f46b4ceaf176d540f4f03ceb47
david.salle <david.salle@ensemblescolaire-niort.com>
2024-10-19 18:19:58 +0200

    Ajout des vérifications de la couleur

--- screen.py
+++ screen.py
@@ -30,7 +30,7 @@ class Screen:
     def get_message(self) -> str:
         return self.message
     
-    def get_color(self) -> tuple:
+    def get_color(self) -> str:
         return self.color
 
     def get_speed(self) -> int:
@@ -41,12 +41,7 @@ class Screen:
 
          
     def set_message(self, new_message: str) -> None:
-        """Modifie le message affiché par l'écran
-         
-        Liste des contraintes générant une erreur
-        - longueur maximum = 50 caractères
-        - uniquement des chiffres et des lettres (minuscules ou majuscules)
-        """
+        """Modifie le message affiché par l'écran"""
         #print( re.search(r"[^ \w+]", new_message) )
         if isinstance(new_message, str) == False:
             raise ScreenError("Message is not a string")
@@ -57,10 +52,17 @@ class Screen:
         else:
             self.message = new_message
     
-    def set_color(self, new_color):
-        self.color = new_color
+    def set_color(self, new_color: str) -> None:
+        """Modifie la couleur du message affiché"""        
+        if isinstance(new_color, str) == False:
+            raise ScreenError("Color is not a string")
+        elif re.match(r"^#([0-9a-fA-F]{6})$", new_color) is None:
+            raise ScreenError("Color must follow the HTML color format")        
+        else:            
+            self.color = new_color
 
     def set_speed(self, new_speed: int) -> None:
+        """Modifie la durée d'affichage du message"""
         if isinstance(new_speed, int) == False:
             raise ScreenError("Speed is not an integer")
         elif new_speed < 5 or new_speed > 20:
@@ -74,8 +76,10 @@ class Screen:
 
 if __name__ == "__main__":
     # Tests unitaires
-    s1 = Screen("...", (255, 0, 0), 10, 1)
+    s1 = Screen("...", "#ff0000", 10, 1)
 
-    s1.set_message("Hello world")
+    #s1.set_message("Hello world")
     #s1.set_message("sdddddddddddddddddddddddddddddddddddddddtggggggggggggggggggggggggggggst")
     #s1.set_message("<script></script>")
+
+    s1.set_color("#00000t")
